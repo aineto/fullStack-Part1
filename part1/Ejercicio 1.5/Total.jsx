1feb9b3cecbe55f593485ae3a7f6c35c485af268
aineto <ainetopro@gmail.com>
2025-02-23 13:27:48 +0100

    upload ejercicio 1.5

--- part1/Ejercicio 1.5/Total.jsx
+++ part1/Ejercicio 1.5/Total.jsx
@@ -2,8 +2,7 @@ const Total = ({ parts }) => {
   return (
     <div>
       <p>
-        Number of exercises:
-        {parts[0].exercises + parts[1].exercises + parts[2].exercises}{' '}
+        Number of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}{' '}
       </p>
     </div>
   );
