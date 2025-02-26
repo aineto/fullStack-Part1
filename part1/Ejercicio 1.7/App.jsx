695b50088f19aae901b3ab6954c2eca43f1f880a
aineto <ainetopro@gmail.com>
2025-02-26 14:43:54 +0100

    update ejercicio 1.7

---
+++ part1/Ejercicio 1.7/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import Header from './Header';
+import Button from './Button';
+import Total from './Total';
+const App = () => {
+  const title = 'Give Feedback';
+  const stadistics = 'Stadistics';
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    console.log('value Good', good);
+    setGood(good + 1);
+  };
+
+  const handleNeutral = () => {
+    console.log('value Neutral', neutral);
+    setNeutral(neutral + 1);
+  };
+
+  const handleBad = () => {
+    console.log('value Bad', bad);
+    setBad(bad + 1);
+  };
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positivos = (good * 100) / total;
+
+  return (
+    <div>
+      <Header title={title} />
+      <Button title={'Good'} onSmash={handleGood} />
+      <Button title={'Neutral'} onSmash={handleNeutral} />
+      <Button title={'Bad'} onSmash={handleBad} />
+
+      <Header title={stadistics} />
+      <Total title={'Good'} count={good} />
+      <Total title={'Neutral'} count={neutral} />
+      <Total title={'Bad'} count={bad} />
+      <Total title={'All'} count={total} />
+
+      <Total title={'Average'} count={average} />
+      <Total title={'Positive'} count={positivos} />
+  
+    </div>
+  );
+};
+
+export default App;
